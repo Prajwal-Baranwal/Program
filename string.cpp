875bd14605f70573846e221bfc33db156112f2da
saurabhdohaiya <84789753+saurabhdohaiya@users.noreply.github.com>
2021-10-26 19:05:19 +0530

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,5 +1,18 @@
 #include "iostream";
 using namespace std;
+
+int xstrlen(char *arr){
+  int i=0;
+  while(*(arr+i)!= '\0'){
+    i++;
+  }
+  return i;
+}
+
 int main(){
-cout<<"string"<<"\n";
+  char str[100];
+  cout<<"Enter a string : ";
+  cin >> str;
+  cout << "\nString length is : " << xstrlen(str);
+  return 0;
 }
